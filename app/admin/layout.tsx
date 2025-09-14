cea493c1368b411d634df7dd6d570712122052ef
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-15 03:08:08 +0545

    login mandatory to access admin page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,30 @@
-import type React from "react"
-import { ProtectedRoute } from "@/components/protected-route"
+import type React from "react";
+import { ProtectedRoute } from "@/components/protected-route";
+import Link from "next/link";
 
 export default function AdminLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return <ProtectedRoute requiredRole="admin">{children}</ProtectedRoute>
+  return (
+    <ProtectedRoute
+      requiredRole="admin"
+      fallbacke={
+        <div className="flex flex-col items-center justify-center min-h-screen text-center">
+          <p className="text-lg font-semibold mb-4">
+            Access denied to this page
+          </p>
+          <Link
+            href="/login"
+            className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition"
+          >
+            Go to Login
+          </Link>
+        </div>
+      }
+    >
+      {children}
+    </ProtectedRoute>
+  );
 }
