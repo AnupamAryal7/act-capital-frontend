ae3c6714fbf298562f721931c8c3c60ab9013856
AnupamAryal7 <anmaryal41@gmail.com>
2025-09-20 22:32:17 +0545

    navitems updated for faqs

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -33,7 +33,7 @@ function AdminDashboard({ children }: { children: ReactNode }) {
     { label: "Students", path: "/admin/students" },
     { label: "Reports", path: "/admin/reports" },
     { label: "Courses", path: "/admin/courses" },
-    { label: "FAQs", path: "/admin/coourses" },
+    { label: "FAQs", path: "/admin/faqs" },
   ];
 
   return (
